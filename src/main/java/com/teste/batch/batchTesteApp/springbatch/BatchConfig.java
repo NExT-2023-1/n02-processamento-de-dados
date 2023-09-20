d519df4cce9622bfe2e3c1fc50269f6f5ce67402
HalleyVeras <halley.veras@gmail.com>
2023-09-20 18:00:57 -0300

    Spring Upload Service

--- src/main/java/com/teste/batch/batchTesteApp/springbatch/BatchConfig.java
+++ src/main/java/com/teste/batch/batchTesteApp/springbatch/BatchConfig.java
@@ -1,7 +1,5 @@
 package com.teste.batch.batchTesteApp.springbatch;
-
 import javax.sql.DataSource;
-
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
@@ -12,15 +10,12 @@ import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourc
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.PathResource;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
---
+++ src/main/java/com/teste/batch/batchTesteApp/springfiles/ResponseMessage.java
@@ -0,0 +1,13 @@
+package com.teste.batch.batchTesteApp.springfiles;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseMessage {
+    
+    private String message;
+}
